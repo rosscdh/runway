2fd83386eeb2d8ca4a679593ad93aec566cd77fc
Ross Crawford-d'Heureuse <rosscdh@s.local>
2015-12-11 15:59:32 +0100

    Beginnings

---
+++ gulp/configure.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+var rename = require("gulp-rename");
+var ngConstant = require('gulp-ng-constant');
+
+
+gulp.task('config:dev', function () {
+  gulp.src('./config/dev.json')
+    .pipe(rename("env.config.js")) // important rename dev to config
+    .pipe(ngConstant({
+      name: 'HiveEmpire.config',
+    }))
+    // Writes config.js to src/app/config.json folder
+    .pipe(gulp.dest('src/app'));
+});
+
+gulp.task('config:staging', function () {
+  gulp.src('./config/staging.json')
+    .pipe(rename("env.config.js")) // important rename prod to config
+    .pipe(ngConstant({
+      name: 'HiveEmpire.config',
+    }))
+    // Writes config.js to src/app/config.json folder
+    .pipe(gulp.dest('src/app'));
+});
+
+
+gulp.task('config:production', function () {
+  gulp.src('./config/production.json')
+    .pipe(rename("env.config.js")) // important rename prod to config
+    .pipe(ngConstant({
+      name: 'HiveEmpire.config',
+    }))
+    // Writes config.js to src/app/config.json folder
+    .pipe(gulp.dest('src/app'));
+});
